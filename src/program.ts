6e671c3516e9b1b0132f1f3626986b1561ee917d
hurihuchi <gmldnr96@gmail.com>
2024-01-30 16:47:40 +0900

    chore: change program name

--- src/program.ts
+++ src/program.ts
@@ -5,7 +5,7 @@ import reactCommand from './commands/react'
 export const program = new Command()
 
 program
-  .name('accio-playground')
+  .name('accio')
   .description('Build and load your code playground easily and quickly by just one command')
   .version('0.0.1')
 
