55ba758ea223e2bed957cc57bd6f69884e6bb5a4
hurihuchi <gmldnr96@gmail.com>
2024-01-31 20:25:09 +0900

    change bin & main field in package.json

--- src/index.ts
+++ src/index.ts
@@ -2,5 +2,4 @@
 
 import { program } from './program'
 
-// log(figlet.textSync('Accio Playground'))
 program.parse(process.argv)
--- src/program.ts
+++ src/program.ts
@@ -1,13 +1,14 @@
 import { Command } from '@commander-js/extra-typings'
 import jsCommand from './commands/javascript'
 import reactCommand from './commands/react'
+import { version } from '../package.json'
 
 export const program = new Command()
 
 program
   .name('accio')
   .description('Build and load your code playground easily and quickly by just one command')
-  .version('0.0.1')
+  .version(version)
 
 // commands
 jsCommand()
